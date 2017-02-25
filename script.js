cb9c6cd158b3f092c69f05734afe849d9286ed2c
amandarama42 <amb417@pitt.edu>
2017-02-25 14:15:27 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,13 @@
+(function() {
+  const form = document.querySelector('#myForm');
+  const dropdown = document.querySelector('#dropdown');
+  const textbox = document.querySelector("#trumpsbutt");
+  const message = "Donald Trump is ";
+
+  function showValue(e) {
+    alert(message + dropdown.value + "\n" + textbox.value);
+    e.preventDefault();
+  }
+
+  form.addEventListener('submit', showValue);
+})();
